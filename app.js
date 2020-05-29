0342a1921d13919c0d770bb770ef563aba42eeff
NILANJAN MAJUMDAR <n.majumder14@gmail.com>
2020-05-29 15:31:20 +0530

    Graphql with mongosb

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
+const Event = require("./models/event");
 
 const app = express();
 
-const events = [];
+
 
 app.use(bodyParser.json());
 
@@ -45,16 +46,24 @@ app.use("/graphql",graphqlHttp({
             return events;
         },
         createEvent: (args) => {
-        const event = {
-            _id : Math.random().toString(),
+            const event = new Event({
             title: args.eventInput.title,
             description:args.eventInput.description,
             price: +args.eventInput.price,
-            date: args.eventInput.date
-        };
-        console.log(args);
-        events.push(event);
-        return event;
+            date: new Date(args.eventInput.date)
+        });
+
+       return event // returning cuzz of async operation for proper result
+       .save()
+       .then(res => {
+           console.log(result);
+           return {...result._doc};
+       })
+       .catch(err => {
+           console.log(err);
+           throw err;
+       });
+       
         }
     },
     graphiql:true
