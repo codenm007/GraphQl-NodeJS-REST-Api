8e302395b5846e55ef6e21fc78c791786b0c9970
NILANJAN MAJUMDAR <n.majumder14@gmail.com>
2020-05-29 12:19:36 +0530

    REST API with graphql types

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const events = [];
+
+app.use(bodyParser.json());
+
+app.use("/graphql",graphqlHttp({
+    schema : buildSchema(`
+    type Event {
+        _id: ID!
+        title:String!
+        description:String!
+        price:Float!
+        date:String!
+    }
+
+    input EventInput {
+        title: String!
+        description : String!
+        price: Float!
+        date:String!
+    }
+
+    type RootQuery{
+      events: [Event!]! 
+    }
+
+    type RootMutation{
+        createEvent(eventInput: EventInput):Event
+    }
+
+    schema {
+        query:RootQuery
+        mutation:RootMutation
+    }
+    `),
+    rootValue:{
+        events: () => {
+            return events;
+        },
+        createEvent: (args) => {
+        const event = {
+            _id : Math.random().toString(),
+            title: args.eventInput.title,
+            description:args.eventInput.description,
+            price: +args.eventInput.price,
+            date: args.eventInput.date
+        };
+        console.log(args);
+        events.push(event);
+        return event;
+        }
+    },
+    graphiql:true
+})
+);
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-0lbti.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
+.then(() => {
+    app.listen(3001);
+    console.log("connected to mongodb successfully !");
+})
+.catch(err => {
+        console.log(`Error code : ${err}`);
+    });
+
